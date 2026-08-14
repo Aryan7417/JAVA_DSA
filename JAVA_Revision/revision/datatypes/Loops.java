56f2332f4f9e3c07bdc1dc953f1f0d193555749f
Aryan7417 <caryan830@gmail.com>
2026-08-14 10:29:05 +0530

    LOOPS

---
+++ JAVA_Revision/revision/datatypes/Loops.java
@@ -0,0 +1,33 @@
+package JAVA_Revision.revision.datatypes;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // while LOOP
+
+        int b = 0;
+        while (b <= 20) {
+            System.out.println("khushi");
+            b++;
+
+        }
+
+        System.out.println("----------for LOOP --------");
+        // //for LOOP
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // do while loop
+
+        System.out.println("-----Do while loop-------");
+
+        int a = 101;
+        do {
+            System.out.println("hey khusi");
+            a++;
+        } while (a <= 101);
+
+    }
+
+}
