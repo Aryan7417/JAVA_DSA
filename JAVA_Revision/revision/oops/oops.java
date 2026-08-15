e0d3895190ac7ca03f7803860b0e959330eaee0f
Aryan7417 <caryan830@gmail.com>
2026-08-15 22:56:27 +0530

    intro OOPS

---
+++ JAVA_Revision/revision/oops/oops.java
@@ -0,0 +1,20 @@
+package JAVA_Revision.revision.oops;
+
+public class oops {
+    public static void main(String[] args) {
+        //class
+        //object
+        //Encapsulation
+        //Inheritance
+        //Polymorphism
+        //Abstraction
+
+        //class
+        //its a bluePrient for a creating object
+        
+        
+
+        
+        
+    }
+}
