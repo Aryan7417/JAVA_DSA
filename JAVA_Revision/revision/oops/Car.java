073c35fc9499e66e1abe6011e8df64dad6d7dc8f
Aryan7417 <caryan830@gmail.com>
2026-08-15 23:19:58 +0530

    example

---
+++ JAVA_Revision/revision/oops/Car.java
@@ -0,0 +1,11 @@
+package JAVA_Revision.revision.oops;
+
+public class Car {
+    int speed ;
+    String color;
+
+    void drive(){
+        System.out.println("driveing");
+    }
+    
+}
---
+++ JAVA_Revision/revision/oops/test.java
@@ -0,0 +1,11 @@
+package JAVA_Revision.revision.oops;
+
+public class test {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.speed = 101;
+        car.color = "green";
+        car.drive();
+    }
+
+}
