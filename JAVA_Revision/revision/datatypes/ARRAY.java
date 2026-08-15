91ff1dac3c80bfc03182acf6eb2b0abdc95d0089
Aryan7417 <caryan830@gmail.com>
2026-08-15 18:38:56 +0530

    Array

---
+++ JAVA_Revision/revision/datatypes/ARRAY.java
@@ -0,0 +1,25 @@
+package JAVA_Revision.revision.datatypes;
+
+public class ARRAY {
+    public static void main(String[] args) {
+
+        // same size
+        // fixed size
+        // array
+
+        // decleration => int a [] = new int[5];
+        // int[] a = new int[5];
+        // System.out.println(a);
+
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println(a[i]);
+        // }
+
+        
+
+         
+       
+
+    }
+
+}
